b0841bbc05404d95a8e0eabc17c3258dec14e1bf
Sonny <s.junhyuck@gmail.com>
2024-01-03 17:07:22 +0900

    [MODIFY] Interpolation Func

--- shader/compute.cs
+++ shader/compute.cs
@@ -15,6 +15,20 @@ layout(std430, binding = 3) buffer lblock { float lifes[]; };
 layout(std430, binding = 4) buffer tblock { vec2 tails[]; };
 layout(std430, binding = 5) buffer tcblock { vec4 tail_colors[]; };
 
+vec2 Interpolation(vec2 v)
+{
+     v -= vec2(0.5f, 0.5f);
+     v *= 2.0f;
+     return v;
+}
+
+vec2 CounterInterpolation(vec2 v)
+{
+     v *= 0.5f;
+     v += vec2(0.5f, 0.5f);
+     return v;
+}
+
 void main ()
 {
      int index = int(gl_GlobalInvocationID);
@@ -27,7 +41,7 @@ void main ()
 
      vec2 position;
      vec2 velocity;
-     vec4 color = texture(particle_texture, vec2(fract(particle_index / particle_res), floor(particle_index / particle_res) / particle_res));
+     vec4 color = texture(particle_texture, vec2(fract(particle_index / particle_res), floor(particle_index / particle_res) / particle_res));  // decoding
      vec2 particle_pos = vec2( color.r / 255.0 + color.b, color.g / 255.0 + color.a);
 
      if(life < age)
@@ -37,7 +51,7 @@ void main ()
 
      if(age == 0)
      {
-          position = vec2(2.0 * particle_pos.x - 1.0, 1.0 - 2.0 * particle_pos.y);
+          position = Interpolation(particle_pos);  // 0 ~ 1 -> -1 ~ 1
      }
      else
      {
@@ -45,19 +59,17 @@ void main ()
      }
 
      // get velocity
-     velocity = texture(wind_texture, position).xy;
+     velocity = texture(wind_texture, CounterInterpolation(position)).rg;  // -1 ~ 1 -> 0 ~ 1
      
      // tail
-     //float opacity = 1.0f;//age / life;
      float opacity = age / life;
      tail_colors[tail_index] = vec4(length(velocity), length(velocity), 1 - length(velocity), min(1, 0.5f + opacity));
      tails[tail_index] = position;  // old position
 
      // position
-     //velocity -= vec2(1.0f, 1.0f);
-     velocity -= vec2(0.5f, 0.5f);
+     velocity = Interpolation(velocity);
      velocity *= delta * delta * 16;
-     positions[index] = position + velocity.xy;  // new position
+     positions[index] = position + velocity;  // new position
      
      // age
      age += delta * 20;
